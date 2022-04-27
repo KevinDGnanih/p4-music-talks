bb97e507ceee9735e739f01bc266c0abfe8f956c
KevinDGnanih <kevin.gnanih@gmail.com>
2022-04-27 23:21:56 +0000

    Linked heroku database as backend

--- django_music_talks/settings.py
+++ django_music_talks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
@@ -78,11 +79,15 @@ WSGI_APPLICATION = 'django_music_talks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
