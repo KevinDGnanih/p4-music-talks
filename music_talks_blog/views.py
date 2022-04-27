c65747fb7d9d495789f0fdf6f590f5b39d9c89ee
KevinDGnanih <kevin.gnanih@gmail.com>
2022-04-27 19:58:44 +0000

    Made migration

--- music_talks_blog/views.py
+++ music_talks_blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'music_talks_blog/blog_home_page.html')
+    return render(request, 'music_talks_blog/home_page.html')
